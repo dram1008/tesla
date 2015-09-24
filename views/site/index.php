1d9e2c4a705bc9db5cb873888b46deddfa6b74f3
dram1008 <dram1008@yandex.ru>
2015-09-24 20:14:01 +0300

    init

--- views/site/index.php
+++ views/site/index.php
@@ -15,6 +15,7 @@ $this->registerJs("$('.carousel').carousel()");
             <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
             <li data-target="#carousel-example-generic" data-slide-to="1"></li>
             <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
         </ol>
 
         <!-- Wrapper for slides -->
@@ -33,6 +34,12 @@ $this->registerJs("$('.carousel').carousel()");
             </div>
             <div class="item">
                 <img src="/images/controller/site/index/3.jpg" alt="...">
+                <div class="carousel-caption">
+                    ...
+                </div>
+            </div>
+            <div class="item">
+                <img src="/images/controller/site/index/4.jpg" alt="...">
                 <div class="carousel-caption">
                     ...
                 </div>
