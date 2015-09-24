430653ad30e7681aa02ff21e5faadde462af0cd3
dram1008 <dram1008@yandex.ru>
2015-09-24 07:32:08 +0300

    init

--- views/site/dostavka.php
+++ views/site/dostavka.php
@@ -14,8 +14,23 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-contact">
     <h1 class="page-header"><?= Html::encode($this->title) ?></h1>
 
+    <div class="row">
+        <div class="col-lg-4">
+            <img src="/images/controller/site/dostavka/1.jpg" width="100%" class="thumbnail"/>
+        </div>
+
+        <div class="col-lg-4">
+            <img src="/images/controller/site/dostavka/2.jpg" width="100%" class="thumbnail"/>
+        </div>
+
+
+    </div>
+    <hr>
+
     <p>Доставка возможна в любую точку планеты</p>
     <p>Отгрузка происходит только после 50% предоплаты.</p>
     <p>Доставка ТК Деловые линии или на Ваш выбор ТК.</p>
     <p>По Москве доставка бесплатная.</p>
+
+
 </div>
