2551ff8340c506305ae597688a01cba037183f2e
dram1008 <dram1008@yandex.ru>
2015-09-25 00:21:08 +0300

    init

--- config/web.php
+++ config/web.php
@@ -3,34 +3,35 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'aliases' => [
-        '@csRoot' => '@vendor/dram1008/library/lib',
+    'id'         => 'basic',
+    'basePath'   => dirname(__DIR__),
+    'bootstrap'  => ['log'],
+    'aliases'    => [
+        '@csRoot' =>  __DIR__ . '/../vendor/dram1008/library/lib',
+        '@web'    =>  __DIR__ . '/../public_html',
     ],
     'components' => [
-        'request' => [
+        'request'      => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey'    => '',
             'enableCookieValidation' => false,
             'enableCsrfValidation'   => false,
         ],
-        'urlManager'           => [
+        'urlManager'   => [
             'enablePrettyUrl'     => true,
             'showScriptName'      => false,
             'enableStrictParsing' => true,
             'suffix'              => '',
             'rules'               => require(__DIR__ . '/urlRules.php'),
         ],
-        'cache' => [
+        'cache'        => [
             'class' => 'yii\caching\FileCache',
         ],
-        'user' => [
-            'identityClass' => 'app\models\User',
+        'user'         => [
+            'identityClass'   => 'app\models\User',
             'enableAutoLogin' => true,
         ],
-        'formatter'            => [
+        'formatter'    => [
             'dateFormat'        => 'dd.MM.yyyy',
             'timeFormat'        => 'php:H:i:s',
             'datetimeFormat'    => 'php:d.m.Y H:i',
@@ -43,19 +44,19 @@ $config = [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer'               => require(__DIR__ . '/mailerTransport.php'),
-        'log' => [
+        'mailer'       => require(__DIR__ . '/mailerTransport.php'),
+        'log'          => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
+            'targets'    => [
                 [
-                    'class' => 'yii\log\FileTarget',
+                    'class'  => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
                 ],
             ],
         ],
-        'db' => require(__DIR__ . '/db.php'),
+        'db'           => require(__DIR__ . '/db.php'),
     ],
-    'params' => $params,
+    'params'     => $params,
 ];
 
 if (YII_ENV_DEV) {
