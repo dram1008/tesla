c8a21e7c9c6d15f7ea5bee7ef9c987eefdcabc69
dram1008 <dram1008@yandex.ru>
2015-09-26 05:35:19 +0300

    init

--- mail/text/new_request.php
+++ mail/text/new_request.php
@@ -16,4 +16,4 @@ Email: <?= $request->getField('email') ?>
 Телефон: <?= $request->getField('phone') ?>
 Комментарий: <?= nl2br($request->getField('comment'))  ?>
 Доставка: <?= $request->getField('point_address') ?>
-<?= \yii\helpers\Url::to(['site/map', 'lat' => $request->getField('point_lat'), 'lng' => $request->getField('point_lng')])?>
+<?= \yii\helpers\Url::to(['site/map', 'lat' => $request->getField('point_lat'), 'lng' => $request->getField('point_lng')], true) ?>
