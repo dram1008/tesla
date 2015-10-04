065efb1d59dfb2061829d3611a7301625280c76b
dram1008 <dram1008@yandex.ru>
2015-10-04 15:12:56 +0300

    init

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,7 +1,7 @@
 <?php
 
-/* @var $this \yii\web\View */
-/* @var $content string */
+/** @var $this \yii\web\View */
+/** @var $content string */
 
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
@@ -10,8 +10,8 @@ use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
-$this->registerMetaTag(['name' => 'keywords', 'content' => 'Генератор Теслы Купить, Арендовать, Бестоплиный генератор, Вечный двигатель, Диллерская программа, Доставка в любую точку планеты, Сервисное обслуживание']);
-$this->registerMetaTag(['name' => 'description', 'content' => 'Генератор Теслы Купить, Арендовать, Бестоплиный генератор, Вечный двигатель, Диллерская программа, Доставка в любую точку планеты, Сервисное обслуживание']);
+$this->registerMetaTag(['name' => 'keywords', 'content' => 'Генератор Теслы Купить, Арендовать, Бестопливный генератор, Вечный двигатель, Диллерская программа, Доставка в любую точку планеты, Сервисное обслуживание']);
+$this->registerMetaTag(['name' => 'description', 'content' => 'Генератор Теслы Купить, Арендовать, Бестопливный генератор, Вечный двигатель, Диллерская программа, Доставка в любую точку планеты, Сервисное обслуживание']);
 $this->registerMetaTag(['name' => 'title', 'content' => $this->title]);
 
 ?>
@@ -23,7 +23,7 @@ $this->registerMetaTag(['name' => 'title', 'content' => $this->title]);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name='yandex-verification' content='6721c497dd2fba14'/>
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?> :: Генератор Теслы Купить, Арендовать, Бестоплиный генератор, Диллерская
+    <title><?= Html::encode($this->title) ?> :: Генератор Теслы Купить, Арендовать, Бестопливный генератор, Диллерская
         программа, Доставка в любую точку планеты, Сервисное обслуживание</title>
     <link rel="shortcut icon" href="/images/ico.png">
     <?php $this->head() ?>
