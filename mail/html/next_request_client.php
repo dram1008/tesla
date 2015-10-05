5ac81ffedb882c8834e05c307b2bc01c8b803cdf
dram1008 <dram1008@yandex.ru>
2015-10-06 01:40:30 +0300

    новая форма заказа редактирование профиля

--- mail/html/next_request_client.php
+++ mail/html/next_request_client.php
@@ -4,6 +4,8 @@
 /** @var $user \app\models\User */
 
 $product = \app\models\Product::find($request->getField('product_id'));
+Yii::info(\yii\helpers\VarDumper::dumpAsString($request), 'tg\\$request');
+Yii::info(\yii\helpers\VarDumper::dumpAsString($product), 'tg\\$product');
 ?>
 
 
