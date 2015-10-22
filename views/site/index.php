1034a0b907b09360842a98d08f7f879687accc35
dram1008 <dram1008@yandex.ru>
2015-10-22 03:58:21 +0300

    subscribe

--- views/site/index.php
+++ views/site/index.php
@@ -13,9 +13,9 @@ $this->registerJs("$('.carousel').carousel()");
         <!-- Indicators -->
         <ol class="carousel-indicators">
 
-            <li data-target="#carousel-example-generic" data-slide-to="2"  class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="0"  class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
         </ol>
 
         <!-- Wrapper for slides -->
