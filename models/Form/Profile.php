bd15fac1d075f7788b78fa15382505cdede726bb
dram1008 <dram1008@yandex.ru>
2015-11-21 14:27:00 +0300

    news

--- models/Form/Profile.php
+++ models/Form/Profile.php
@@ -44,6 +44,7 @@ class Profile extends BaseForm
     public $birth_town;
     public $zvezdnoe;
     public $referal_code;
+    public $mission;
 
     public $subscribe_is_news;
     public $subscribe_is_site_update;
