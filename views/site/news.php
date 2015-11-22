72640b6ca15b98ee7d4f17f45285ac82a490ae6c
dram1008 <dram1008@yandex.ru>
2015-11-22 14:45:01 +0300

    news

--- views/site/news.php
+++ views/site/news.php
@@ -11,13 +11,13 @@ $this->title = 'Новости';
         <?php foreach(\app\models\NewsItem::query()->orderBy(['date_insert' => SORT_DESC])->all() as $item) { ?>
             <?php $i = new \app\models\NewsItem($item); ?>
             <div class="col-lg-4">
+                <div style="height: 70px;">
+                    <p><?= $i->getField('header')?></p>
+                </div>
                 <p style="font-size: 70%;color: #888;"><?= \app\services\GsssHtml::dateString($i->getField('date')) ?></p>
                 <a href="<?= $i->getLink() ?>">
                     <img src="<?= $i->getImage()?>" width="100%" class="thumbnail"/>
                 </a>
-            </div>
-            <div class="col-lg-8">
-                <p><?= $i->getField('description')?></p>
             </div>
         <?php } ?>
     </div>
